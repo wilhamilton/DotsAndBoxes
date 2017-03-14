c30a24c3abf5c701abf9fb4aaa8d16554695bbb9
Wil Hamilton <wil.hamilton@gmail.com>
2017-03-14 11:59:23 -0400

    updated Dot class to conform to PEP 8
    Updated naming of class methods and internal variables (parameters).  Updated documentation to provide more insight to the functionality contained within the dot class.

--- Game/Dot.py
+++ Game/Dot.py
@@ -1,10 +1,27 @@
 """
 A Dot on the gameboard.
 
-This class represents a dot.  A dot has 3 properties: a vertical edge, a
-horizontal edge, and an owner.
+This class represents a dot. Dots are also responsible for creating edges and
+squares in the game.  As such a dot has 3 properties:
+    - a vertical edge
+    - a horizontal edge
+    - an owner (defining is as a square)
 
-Every dot is the upper-left corner of a square (and the corresponding) edges.
+Every dot is the upper-left corner of the square it defines.  It is also the
+left end point of the horizontal edge it defines and the upper end point
+of the vertical edge it defines.
+
+The structure of a single dot is as follows:
+
+    O--
+    |S
+
+Where O represents the dot, -- a horizontal edge, | a vertical edge, and S
+represents a square.
+
+The logic for determining whether a dot should define a square is
+contained in the game board class.  A dot simply stores whether or not it
+is a square.
 """
 
 
@@ -16,12 +33,14 @@ class Dot:
         Initialize a dot.
 
         A dot starts with no edges and no owner (associated square).
+        A dot does not need to know its own position.  This data is maintained
+        by the game board class.
         """
         self.vertical = None      # we do not start with a vertical edge
         self.horizontal = None    # we do not start with a horizontal edge
         self.owner = None         # we do not have an owner (not a square)
 
-    def setVerticalEdge(self, playerId):
+    def set_vertical_edge(self, player_id):
         """
         Set the status for this dot as having a vertical edge.
 
@@ -30,12 +49,13 @@ class Dot:
         if the edge was already defined.
         """
         if self.vertical is None:
-            self.vertical = playerId
+            # this dot does not have a vertical edge, create one
+            self.vertical = player_id
             return True
         else:
             return False
 
-    def setHorizontalEdge(self, playerId):
+    def set_horizontal_edge(self, player_id):
         """
         Set the status for this dot as having a horizontal edge.
 
@@ -44,12 +64,13 @@ class Dot:
         if the edge was already defined.
         """
         if self.horizontal is None:
-            self.horizontal = playerId
+            # this dot does not have a horizontal edge, create one
+            self.horizontal = player_id
             return True
         else:
             return False
 
-    def setOwner(self, playerId):
+    def set_as_square(self, player_id):
         """
         Set the owner of the square this dot defines.
 
@@ -58,7 +79,8 @@ class Dot:
         dot/square is already owned.
         """
         if self.owner is None:
-            self.owner = playerId
+            # this dot does not yet define a squre, update it so it does
+            self.owner = player_id
             return True
         else:
             return False
